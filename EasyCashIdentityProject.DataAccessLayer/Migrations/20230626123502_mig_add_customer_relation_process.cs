81dde039ac81769e74638bce91a4bd8f253e912d
mertcansglam35 <mertcansaglam35@hotmail.com>
2023-06-26 17:29:17 +0300

    update

---
+++ EasyCashIdentityProject.DataAccessLayer/Migrations/20230626123502_mig_add_customer_relation_process.cs
@@ -0,0 +1,291 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EasyCashIdentityProject.DataAccessLayer.Migrations
+{
+    public partial class mig_add_customer_relation_process : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CustomerAccountNumber",
+                table: "CustomerAccounts",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CustomerAccountCurrency",
+                table: "CustomerAccounts",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "CustomerAccountBalance",
+                table: "CustomerAccounts",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BankBranch",
+                table: "CustomerAccounts",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProcessType",
+                table: "CustomerAccountProcesses",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Amount",
+                table: "CustomerAccountProcesses",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ReceiverID",
+                table: "CustomerAccountProcesses",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SenderID",
+                table: "CustomerAccountProcesses",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Surname",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUrl",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "District",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ConfirmCode",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "City",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CustomerAccountProcesses_ReceiverID",
+                table: "CustomerAccountProcesses",
+                column: "ReceiverID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CustomerAccountProcesses_SenderID",
+                table: "CustomerAccountProcesses",
+                column: "SenderID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CustomerAccountProcesses_CustomerAccounts_ReceiverID",
+                table: "CustomerAccountProcesses",
+                column: "ReceiverID",
+                principalTable: "CustomerAccounts",
+                principalColumn: "CustomerAccountID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CustomerAccountProcesses_CustomerAccounts_SenderID",
+                table: "CustomerAccountProcesses",
+                column: "SenderID",
+                principalTable: "CustomerAccounts",
+                principalColumn: "CustomerAccountID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CustomerAccountProcesses_CustomerAccounts_ReceiverID",
+                table: "CustomerAccountProcesses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CustomerAccountProcesses_CustomerAccounts_SenderID",
+                table: "CustomerAccountProcesses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CustomerAccountProcesses_ReceiverID",
+                table: "CustomerAccountProcesses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CustomerAccountProcesses_SenderID",
+                table: "CustomerAccountProcesses");
+
+            migrationBuilder.DropColumn(
+                name: "ReceiverID",
+                table: "CustomerAccountProcesses");
+
+            migrationBuilder.DropColumn(
+                name: "SenderID",
+                table: "CustomerAccountProcesses");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CustomerAccountNumber",
+                table: "CustomerAccounts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CustomerAccountCurrency",
+                table: "CustomerAccounts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "CustomerAccountBalance",
+                table: "CustomerAccounts",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BankBranch",
+                table: "CustomerAccounts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProcessType",
+                table: "CustomerAccountProcesses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Amount",
+                table: "CustomerAccountProcesses",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Surname",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUrl",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "District",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ConfirmCode",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "City",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
