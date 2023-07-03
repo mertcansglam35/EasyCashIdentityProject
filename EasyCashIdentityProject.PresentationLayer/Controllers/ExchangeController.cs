ccc7e99e0557b6ceafee0c42bf8780ec419eb6ef
mertcansglam35 <mertcansaglam35@hotmail.com>
2023-07-03 21:08:27 +0300

    added MyLastProcessController With Index.cshtml For Exchange update

--- EasyCashIdentityProject.PresentationLayer/Controllers/ExchangeController.cs
+++ EasyCashIdentityProject.PresentationLayer/Controllers/ExchangeController.cs
@@ -22,8 +22,8 @@ namespace EasyCashIdentityProject.PresentationLayer.Controllers
             using (var response = await client.SendAsync(request))
             {
                 response.EnsureSuccessStatusCode();
-                var body = await response.Content.ReadAsStringAsync();
-                ViewBag.UsdToTry = body;
+                var body = await response.Content.ReadAsStringAsync() + "00000";
+                ViewBag.UsdToTry = body.Substring(0, 7);
             }
             #endregion
 
@@ -42,8 +42,8 @@ namespace EasyCashIdentityProject.PresentationLayer.Controllers
             using (var response2 = await client2.SendAsync(request2))
             {
                 response2.EnsureSuccessStatusCode();
-                var body2 = await response2.Content.ReadAsStringAsync();
-                ViewBag.EurToTry = body2;
+                var body2 = await response2.Content.ReadAsStringAsync() + "00000";
+                ViewBag.EurToTry = body2.Substring(0, 7);
             }
             #endregion
 
@@ -62,8 +62,28 @@ namespace EasyCashIdentityProject.PresentationLayer.Controllers
             using (var response3 = await client3.SendAsync(request3))
             {
                 response3.EnsureSuccessStatusCode();
-                var body3 = await response3.Content.ReadAsStringAsync();
-                ViewBag.GbpToTry = body3;
+                var body3 = await response3.Content.ReadAsStringAsync() + "00000";
+                ViewBag.GbpToTry = body3.Substring(0, 7);
+            }
+            #endregion
+
+            #region
+            var client4 = new HttpClient();
+            var request4 = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://currency-exchange.p.rapidapi.com/exchange?from=USD&to=EUR&q=1.0"),
+                Headers =
+            {
+                { "X-RapidAPI-Key", "bcf3b18f90mshf0d92829290b58fp161550jsn4c426b043e49" },
+                { "X-RapidAPI-Host", "currency-exchange.p.rapidapi.com" },
+            },
+            };
+            using (var response4 = await client4.SendAsync(request4))
+            {
+                response4.EnsureSuccessStatusCode();
+                var body4 = await response4.Content.ReadAsStringAsync() + "00000";
+                ViewBag.UsdToEur = body4.Substring(0, 7);
             }
             #endregion
 
